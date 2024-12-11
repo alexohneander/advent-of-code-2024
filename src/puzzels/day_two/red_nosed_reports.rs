200fa0be0dcbba748368fa9adc24802d3901743b
Alex Wellnitz <moin@wellnitz-alex.de>
2024-12-11 21:55:19 +0100

    fix: add comments

--- src/puzzels/day_two/red_nosed_reports.rs
+++ src/puzzels/day_two/red_nosed_reports.rs
@@ -3,12 +3,15 @@ use crate::helpers::file_helper;
 pub fn solve() {
     println!("Solving Puzzel Day Two");
 
+    // First get the Input as String
     let content = file_helper::get_string_input_from_file("./input/day_two/input.txt");
     println!("Content Length: {}", content.lines().count());
 
+    // Define counter for our result
     let mut safe_line_counter = 0;
     let mut unsafe_line_counter = 0;
 
+    // Last but not least, check if the line is Safe or not
     for line in content.lines() {
         if is_line_safe(line){
             safe_line_counter +=1;
@@ -17,6 +20,7 @@ pub fn solve() {
         }
     }
 
+    // Tada.. our Result
     println!("Safe Lines: {}", safe_line_counter);
     println!("Unsafe Lines: {}", unsafe_line_counter);
 }
