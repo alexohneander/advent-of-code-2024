e9a565030ea11088898293f288f2a7c75311b632
Alex Wellnitz <moin@wellnitz-alex.de>
2024-12-11 17:59:56 +0100

    feat: start solving day two

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use advent_of_code_2024::day_one::historian_hysteria;
+use advent_of_code_2024::{day_one::historian_hysteria, day_two::red_nosed_reports};
 
 fn main() {
-    historian_hysteria::solve();
-    historian_hysteria::solve_part_two();
+    const DAY: i32 = 2; // Define the Puzzle
+
+    match DAY{
+        1 => {
+            historian_hysteria::solve();
+            historian_hysteria::solve_part_two();
+        },
+        2 => {
+            red_nosed_reports::solve();
+        },
+        _=>println!("Out of Index"),
+    }
+    
 }
